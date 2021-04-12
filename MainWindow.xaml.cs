5baaeac92d992cd76d22511135adf7480cc49114
theonlywisdom <mailbaah@gmail.com>
2021-04-12 12:34:09 +0100

    Added method to set up game.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,63 @@ namespace NoughtsAndCrossesGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+        /// <summary>
+        /// Holds the current results of cells in the active game
+        /// </summary>
+        private MarkType[] mResults;
+
+        /// <summary>
+        /// True if it is Player1's turn(X) or Player2's turn (O)
+        /// </summary>
+        private bool mPlayer1Turn;
+
+        /// <summary>
+        /// True if game has ended
+        /// </summary>
+        private bool mGameEnded;
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            NewGame();
+        }
+
+        #endregion
+
+        private void NewGame()
+        {
+            // Create a new blank array of free cells
+            mResults = new MarkType[9];
+
+            for (var i = 0; i < mResults.Length; i++)
+            {
+                mResults[i] = MarkType.Free;
+            }
+
+            // Make sure Player1 starts the game
+            mPlayer1Turn = true;
+
+            // Cast container UI elements to a button
+            // Add iterate every button on the grid
+            Container.Children.Cast<Button>().ToList().ForEach(
+                button =>
+                {
+                    // Change background, foreground and content to default values
+                    button.Content = string.Empty;
+                    button.Background = Brushes.White;
+                    button.Foreground = Brushes.Blue;
+                });
+
+            // Make sure the game hasn't finished
+            mGameEnded = false;
         }
     }
 }
---
+++ MarkType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoughtsAndCrossesGame
+{
+    /// <summary>
+    /// Current cell type in the game
+    /// </summary>
+    public enum MarkType
+    {
+        Free,
+        Nought,
+        Cross,
+    }
+}
